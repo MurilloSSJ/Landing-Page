b14e6f4f147f621d6c5df79629ec8e2dc0a64463
MurilloSSJ <murillostore@gmail.com>
2021-09-03 22:48:28 -0300

    Update Images and Creation How i help section

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+var star = document.createElement("div")
+var heaven = document.querySelector('.heaven')
+heaven.appendChild(star)
+star.style = "width:3px; height:3px; border-radius:50%; box-shadow:50px 30px white; position:absolute;"
+console.log(star)
